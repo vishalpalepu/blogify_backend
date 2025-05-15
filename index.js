e01be2d78a0629a41361efd6ce54b917b86fe450
vishalpalepu <vishal.cinpalepu@gmail.com>
2025-05-15 10:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const blogRoutes = require("./Routes/blog");
 app.use("/api/users", userRoutes);
 app.use("/api/blogs", blogRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
